644c411da2462ee160f04195b5d4d69f44578662
Martin M <mxa.code@gmail.com>
2018-10-26 01:42:52 +0300

    build: add eslint config + linting script

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const prettierOptions = require('./.prettierrc');
+
+const OFF = 0;
+const ERROR = 2;
+
+module.exports = {
+  root: true,
+  'extends': [
+    'prettier'
+  ],
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true
+  },
+  'plugins': [
+    'prettier'
+  ],
+  'rules': {
+    'prettier/prettier': [ERROR, prettierOptions],
+    'arrow-body-style': OFF,
+    'arrow-parens': OFF,
+    'camelcase': OFF,
+    'class-methods-use-this': OFF,
+    'comma-dangle': OFF,
+    'function-paren-newline': OFF,
+    'object-curly-newline': OFF,
+    'implicit-arrow-linebreak': OFF
+  }
+};
