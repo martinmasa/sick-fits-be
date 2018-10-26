d621845f747869758be979a9c9a133edff9e4fe2
Martin M <mxa.code@gmail.com>
2018-10-26 21:20:52 +0300

    feat: add logic to create +  fetch items using prisma + graphql-yoga

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, context, info) {
+    // TODO check if they are logged in
+
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = mutations;
