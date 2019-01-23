61e2b23ea44d0c3f3ceed0425b994c73c00f5a2e
Martin M <mxa.code@gmail.com>
2019-01-23 15:40:12 +0000

    feat: add query for currently logged in user

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,15 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
+  itemsConnection: forwardTo('db'),
+  me(parent, args, context, info) {
+    if (!context.request.userId) {
+      return null;
+    }
+
+    console.log(context.request.userId);
+    return context.db.query.user({ where: { id: 'cjr6qvzonhfuo0950jojojkiw' } }, info);
+  }
 };
 
 module.exports = Query;
